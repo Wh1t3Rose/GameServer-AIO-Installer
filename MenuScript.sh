1dcf64a2903ecde244e71750145f3be65f2657c2
Tyler Cox <ktylerc92@gmail.com>
2018-02-06 23:59:15 -0500

    Create Surf Plugin Menu

--- MenuScript.sh
+++ MenuScript.sh
@@ -28,7 +28,7 @@ fi
 
 # Check root
 if [ $(id -u) -ne 0 ]; then 
-  echo -e "{$RED}Ur not Root bro${NC}"; 
+  echo -e "{$RED}Ur not Root bro!{$NC}"; 
   exit 1; 
 fi
 
@@ -181,7 +181,85 @@ done
 
 if [[ $opt="Retakes" ]]; then
   echo -e "Retakes Server Install starting";
-
+  
+  cmd=(dialog --separate-output --checklist "Select the plugins you want installed:" 22 76 16)
+options=(1 "SurfTimer - {$RED}2.02{$NC} - Core of this server." on
+         2 "AutoFileLoader - Caches all material, model, and sound files for players to download." on # any option can be set to default to "on"
+         3 "Dynamic - PreReq for many plugins to work properly." on
+         4 "Mapchooser_Extended - {$RED}1.10.0{$NC} - Creates map vote at the end of each match. Controlled by maplist.cfg and mapcycle.cfg." on
+         5 "SlopeFix - Smooths out ramps and prevents clipping with player models. (Eg. no more sudden stopping when surfing)" on
+         6 "ServerAdvertisements - Used to greet joining players and post information via chat box" off
+         7 "CallAdmin - Allows players to report players in game to your Discord/TS server. {$RED}Requires Discord_API{$NC}" off
+         8 "csgo_gun_menu - Allows users to pick a gun and a pistol while surfing." off
+         9 "Discord_API - Allows plugins to tie into your discord server. {$RED}Required if using CallAdmin{$NC}" off)
+         10 "Nominations_Extended - Use with MapChooser_Extended to allow players to nominate maps." off
+         11 "sm_skinchooser - Used to allow players to equip models (aka skins for their player model)" off
+         12 "Store - Allows players to purchase pets, hats, masks, etc. This requires a FastDL!" off
+         13 "csgo_gun_menu - Allows users to pick a gun and a pistol while surfing." off
+         14 "thirdperson - Allows players to view their player model in thirdperson." off
+         15 "csgo_gun_menu - Allows users to pick a gun and a pistol while surfing." off
+choices=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
+clear
+for choice in $choices
+do
+    case $choice in
+        1)
+            echo "SurfTimer - {$RED}2.02{$NC} - Core of this server."
+            ;;
+        2)
+            echo "AutoFileLoader - Caches all material, model, and sound files for players to download."
+            ;;
+        3)
+            echo "Dynamic - PreReq for many plugins to work properly."
+            ;;
+        4)
+            echo "Mapchooser_Extended - {$RED}1.10.0{$NC} - Creates map vote at the end of each match. Controlled by maplist.cfg and mapcycle.cfg."
+            ;;
+        4)
+            echo "SlopeFix - Smooths out ramps and prevents clipping with player models. (Eg. no more sudden stopping when surfing)"
+            ;;
+        4)
+            echo "Dynamic - PreReq for many plugins to work properly."
+            ;;
+        4)
+            echo "Dynamic - PreReq for many plugins to work properly."
+            ;;
+        4)
+            echo "Dynamic - PreReq for many plugins to work properly."
+            ;;
+        4)
+            echo "Dynamic - PreReq for many plugins to work properly."
+            ;;
+        4)
+            echo "Dynamic - PreReq for many plugins to work properly."
+            ;;
+        4)
+            echo "Dynamic - PreReq for many plugins to work properly."
+            ;;
+        4)
+            echo "Dynamic - PreReq for many plugins to work properly."
+            ;;
+        4)
+            echo "Dynamic - PreReq for many plugins to work properly."
+            ;;
+        4)
+            echo "Dynamic - PreReq for many plugins to work properly."
+            ;;
+        4)
+            echo "Dynamic - PreReq for many plugins to work properly."
+            ;;
+        4)
+            echo "Dynamic - PreReq for many plugins to work properly."
+            ;;
+    esac
+done
+if [ $opt != "KillingFloor2" ]; then
+  echo "Adding i386 architecture..."
+  dpkg --add-architecture i386 >/dev/null
+  if [ "$?" -ne "0" ]; then
+    echo "ERROR: Cannot add i386 architecture..."
+    exit 1
+  fi
 fi
 done
 
